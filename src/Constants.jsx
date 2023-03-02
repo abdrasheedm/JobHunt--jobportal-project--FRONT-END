6d834b220950bdbc916cccefa3470c29b0b44690
abdrasheedm <rasheedvaniyannurrashi175@gmail.com>
2023-03-02 12:46:54 +0530

    hostingg

--- src/Constants.jsx
+++ src/Constants.jsx
@@ -1,5 +1,5 @@
-export const BASEURL = 'http://10.4.0.85:8000'
+export const BASEURL = 'https://jobhunt.tech'
 // export const BASEURL = 'http://127.0.0.1:8000'
 // export const BASEURL = 'http://192.168.0.109:8000'
-export const WsURL = 'ws://10.4.0.85:8000/ws/notification/'
+export const WsURL = 'wss://jobhunt.tech/ws/notification/'
 // export const WsURL = 'ws://192.168.0.109:8000/ws/notification/'
